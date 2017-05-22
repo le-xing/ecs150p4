603cc60c1b0c3251abde838c235906de9ad5401c
Lena Tan <lentan@ucdavis.edu>
2017-05-22 15:16:54 -0700

    File system data structures implemented

--- libfs/fs.c
+++ libfs/fs.c
@@ -2,12 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "disk.h"
 #include "fs.h"
 
+typedef struct rootEntry* rootEntry_t
+
+struct __attribute__((__packed__)) superblock {
+    char signature[8];
+    uint16_t numBlocks;
+    uint16_t root;
+    uint16_t data;
+    uint16_t numDataBlocks;
+    uint8_t numFATBlocks;
+    char padding[4079];
+};
+
+struct __attribute__((__packed__)) FAT {
+    uint16_t *entries;
+};
+
+struct __attribute__((__packed__)) rootEntry {
+    char filename[16];
+    uint32_t size;
+    uint16_t firstBlock;
+    char padding[10];
+}
+
+struct __attribute__((__packed__)) rootDirectory {
+    rootEntry_t entries[128];
+};
+
+
 int fs_mount(const char *diskname)
 {
+    if(block_disk_open(diskname)) {
+        return -1;
+    }
+
 	return 0;
 }
 
