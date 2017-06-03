ba8b739c2f836ad750a9982c1632b67dc9cea45d
Christopher Mai <cmai@pc47.cs.ucdavis.edu>
2017-06-02 19:45:35 -0700

    fs_write() implemented

--- libfs/fs.c
+++ libfs/fs.c
@@ -11,6 +11,11 @@
 
 #define FAT_EOC 0xFFFF
 
+typedef enum {
+    READ,
+    WRITE,
+} rwFlag;
+
 typedef struct rootEntry* rootEntry_t;
 typedef struct superblock* superblock_t;
  
@@ -43,7 +48,6 @@ struct superblock sblock;
 struct rootEntry root[128];
 struct fileDescriptor fileDescriptors[FS_OPEN_MAX_COUNT];
 uint16_t *fat = NULL;
-void* data;
 int fatFree = 0;
 int rootFree = 0;
 int numOpen = 0;
@@ -55,21 +59,28 @@ superblock_t initSuperblock() {
     return &sblock;
 }
 
-int find_remainingBytes(int fd, int i) //find remainingBytes from offset until end of file of a file identified by a given fd
-{
-	return root[i].size - fileDescriptors[fd].offset;
-}
-
-int find_dataBlock(int fd, int *bytesToRead)
-{
+// Find index of datablock corresponding to offset and find # of bytes to modify
+int find_dataBlock(int fd, int *bytesToModify, rwFlag rw)
+{ 
 	for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
     	if(!strcmp(root[i].filename, fileDescriptors[fd].filename)) { // Find the associated root entry to get file size
-			int remainingBytes = find_remainingBytes(fd, i); // Bytes that can be read 
-			if (*bytesToRead > remainingBytes)
-				*bytesToRead = remainingBytes;
-			return superblock->data + root[i].firstBlock + (fileDescriptors[fd].offset / BLOCK_SIZE); // First block to read from
+			int remainingBytes = root[i].size - fileDescriptors[fd].offset; // Bytes until end of file
+			if(rw == READ) {
+                if (*bytesToModify > remainingBytes)
+				    *bytesToModify = remainingBytes;
+            }
+            if (rw == WRITE && root[i].firstBlock == FAT_EOC && fatFree != 0) { // Empty file with no associated data blocks
+                for (int j = 0; j <  superblock->numDataBlocks; j++) {
+                    if (fat[j] == 0) {
+                        root[i].firstBlock = j;
+                        break;
+                    }
+                }
+            }
+			return superblock->data + root[i].firstBlock + (fileDescriptors[fd].offset / BLOCK_SIZE);
         }
     }
+
     return 0;
 }
 
@@ -80,39 +91,27 @@ int fs_mount(const char *diskname)
         return -1;
     }
 
-    // Read the superblock
+    // Read the superblock, FAT blocks, and root block
     superblock = initSuperblock();
     block_read(0, (void*)superblock);
-    if (memcmp(superblock->signature, "ECS150FS", 8)) {
+    if (memcmp(superblock->signature, "ECS150FS", 8)) { // Check signature of file system
        return -1;
     }
-    // Read FAT blocks
-    fat = (uint16_t*)malloc(superblock->numDataBlocks*sizeof(uint16_t));
 
+    fat = (uint16_t*)malloc(superblock->numDataBlocks*sizeof(uint16_t));
     for (int i = 1; i < superblock->root; i++) {
         block_read(i, ((void*)fat) + BLOCK_SIZE*(i - 1));
     }
 
-    // Read the root block
     block_read(superblock->root, (void*)root);
 
-//TODO: is this even needed?
-    // Read data blocks
-    data = malloc(superblock->numDataBlocks*BLOCK_SIZE);
-    for (int i = superblock->data; i < superblock->data + superblock->numDataBlocks; i++) {
-        int increment = 0;
-        block_read(i, (void*)(data + BLOCK_SIZE*increment));
-        increment++;
-    }
-
-    // Count available entries in FAT
-   for (int i = 0; i < superblock->numDataBlocks; i++) {
+    // Count available entries in FAT and in root
+    for (int i = 0; i < superblock->numDataBlocks; i++) {
         if(fat[i] == 0) {
             fatFree++;
         }
     }
 
-    // Count available entries in root
     for (int j = 0; j < FS_FILE_MAX_COUNT; j++) {
         if(root[j].filename[0] == 0) {
             rootFree++;
@@ -125,21 +124,19 @@ int fs_mount(const char *diskname)
         fileDescriptors[k].fd = -1;
         fileDescriptors[k].offset = -1;
     }
-
 	return 0;
 }
 
 int fs_umount(void)
 {
-    //TODO: write to all blocks, or just modified ones? inside umount() or inside functions that will modify blocks?
     block_write(superblock->root, (void*)root);
 
+    // Free allocated memory
     for (int k = 0; k < FS_OPEN_MAX_COUNT; k++) {
         free(fileDescriptors[k].filename);
     }
 
-    free(data);
-    free(fat); // segfault, TODO: will fix later, has to do with block_read during mount, refer to piazza @879
+    free(fat);
     
     // Close the disk
     if (block_disk_close()) {
@@ -164,6 +161,7 @@ int fs_info(void)
 
 int fs_create(const char *filename)
 {
+    // Don't create if no more space on disk or @filename is invalid
     if (rootFree == 0 || strlen(filename) > FS_FILENAME_LEN || filename[strlen(filename)] != '\0')
         return -1;
 
@@ -175,11 +173,11 @@ int fs_create(const char *filename)
 
     // Create empty file and add root entry
     for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
-        if(root[i].filename[0] == 0) { // find the first empty root entry
+        if(root[i].filename[0] == 0) { // Find the first empty root entry
             strcpy(root[i].filename, filename);
             root[i].size = 0;
             root[i].firstBlock = FAT_EOC;
-            block_write(superblock->root, (void*) root);
+            block_write(superblock->root, (void*) root); // Write change to disk
             return 0;
         }
     }
@@ -188,33 +186,46 @@ int fs_create(const char *filename)
 	return -1; 
 }
 
-//TODO: Test delete on non empty file, check info after (fatFree specifically)
 int fs_delete(const char *filename)
 {
+    void* bounceBuffer = NULL;
+
+    // Check if @filename is valid
     if (strlen(filename) > FS_FILENAME_LEN || filename[strlen(filename)] != '\0')
         return -1;
 
-    // Don't delete file if it is open
+    // Don't delete the file if it is open
     for (int k = 0; k < FS_OPEN_MAX_COUNT; k++) {
         if(fileDescriptors[k].filename && !strcmp(fileDescriptors[k].filename, filename))
             return -1;
     }
- 
-    // TODO: reset FAT entries and clear associated data blocks
-    // don't need to free the data block, just clear it somehow (use memset)
-    // remember to write to disk (blocks were changed)
 
-   // Reset the associated root entry
+    bounceBuffer = malloc(BLOCK_SIZE); 
+
+   // Reset the associated fat entries, data blocks, and root entry
    for(int i = 0; i < FS_FILE_MAX_COUNT; i++) {
         if(!strcmp(root[i].filename, filename)) {
+            int clearIndex = root[i].firstBlock;
+            while(clearIndex != FAT_EOC) {
+                block_read(superblock->data + clearIndex, bounceBuffer);
+                memset(bounceBuffer, 0, BLOCK_SIZE);
+                block_write(superblock->data + clearIndex, bounceBuffer);
+                int next = fat[clearIndex];
+                fat[clearIndex] = 0;
+                fatFree++;
+                clearIndex = next;
+            }
+            free(bounceBuffer);
             root[i].filename[0] = 0;
             root[i].size = 0;
             root[i].firstBlock = FAT_EOC;
+            block_write(superblock->root, (void*) root);
+            rootFree++;
             return 0;
         }
     }
 
-    // Function should not reach here
+    // Function should not reach here if deletion successful
 	return -1;
 }
 
@@ -233,12 +244,13 @@ int fs_open(const char *filename)
 {
     int fileExists = 0;
 
+    // Don't open if disk cannot open any more files or @filename is invalid
     if (numOpen == FS_OPEN_MAX_COUNT || strlen(filename) > FS_FILENAME_LEN || filename[strlen(filename)] != '\0') 
        return -1;
  
     // Check if the file exists on the disk 
     for(int i = 0; i <= FS_FILE_MAX_COUNT; i++) {
-        if(!strcmp(root[i].filename, filename)){ // file found
+        if(!strcmp(root[i].filename, filename)){ // File found
             fileExists = 1;
             break;
         }
@@ -251,7 +263,7 @@ int fs_open(const char *filename)
     for(int k = 0; k < FS_OPEN_MAX_COUNT; k++){
         if (fileDescriptors[k].fd == -1) {
             strcpy(fileDescriptors[k].filename, filename);
-            fileDescriptors[k].fd = k;
+            fileDescriptors[k].fd = k; // Given file descriptor is simply the index in fileDescriptors
             fileDescriptors[k].offset = 0;
             numOpen++;
             return fileDescriptors[k].fd;
@@ -264,7 +276,7 @@ int fs_open(const char *filename)
 
 int fs_close(int fd)
 {
-    // Check if @fd valid and file with @fd is open
+    // Check if @fd is valid and file with @fd is open
     if (fd  >= FS_OPEN_MAX_COUNT || fd < 0 || fileDescriptors[fd].fd != fd) {
         return -1;
     }
@@ -307,53 +319,136 @@ int fs_lseek(int fd, size_t offset)
 
 int fs_write(int fd, void *buf, size_t count)
 {
-          
-	return 0;
+    int* bytesToWrite = (int *)malloc(sizeof(count));
+    int writeBlock, blockBytes, blockOffset, bytesWritten = 0;
+    void* bounceBuffer = malloc(BLOCK_SIZE);
+    if (fd >= FS_OPEN_MAX_COUNT || fd < 0 || fileDescriptors[fd].fd != fd) {
+            return -1;
+    }
+
+    *bytesToWrite = count;
+    writeBlock = find_dataBlock(fd, bytesToWrite, WRITE);
+    blockOffset = fileDescriptors[fd].offset - (fileDescriptors[fd].offset/BLOCK_SIZE)*BLOCK_SIZE;
+
+    // Stop if no more space on disk to write
+    if (writeBlock == FAT_EOC)
+        return bytesWritten;
+
+    if (blockOffset != 0) { // Offset is in middle of a block
+        block_read(writeBlock, bounceBuffer);
+        blockBytes = (fileDescriptors[fd].offset/BLOCK_SIZE + 1)*BLOCK_SIZE - fileDescriptors[fd].offset; // Bytes that can be written in first block
+        if (*bytesToWrite < blockBytes) { // Only need to write to this one block
+            memcpy(bounceBuffer + blockOffset, buf, *bytesToWrite); 
+            bytesWritten += *bytesToWrite; 
+            *bytesToWrite -= *bytesToWrite; // bytesToWrite = 0
+            block_write(writeBlock, bounceBuffer);
+        }
+        else { // Write to the end of the block
+            memcpy(bounceBuffer + blockOffset, buf, blockBytes);
+            bytesWritten += blockBytes;
+            *bytesToWrite -= blockBytes;
+            buf += blockBytes;
+            block_write(writeBlock, bounceBuffer);
+            writeBlock++;
+            memset(bounceBuffer, 0 , BLOCK_SIZE); // Clear the bounce buffer for possible later use
+        }
+    }
+
+    // Write to whole blocks until no longer possible
+    while (*bytesToWrite >= BLOCK_SIZE) {
+        block_read(writeBlock, bounceBuffer);
+        memcpy(bounceBuffer, buf, BLOCK_SIZE);
+        *bytesToWrite -= BLOCK_SIZE;
+        bytesWritten += BLOCK_SIZE;
+        buf += BLOCK_SIZE;
+        block_write(writeBlock, bounceBuffer);
+        memset(bounceBuffer, 0, BLOCK_SIZE);
+        // Allocate more data blocks while needed
+        if (fat[writeBlock - superblock->data] == FAT_EOC && bytesToWrite > 0) {
+            if (fatFree == 0) // There is no more space on the disk to write to
+                return bytesWritten;
+            else { // Find the first free fat block and add it to the file's chainmap
+                for (int i = 0; i < superblock->numDataBlocks; i++) {
+                    if(fat[i] == 0) {
+                        fat[writeBlock - superblock->data] = i;
+                        fat[i] = FAT_EOC;
+                        fatFree--;
+                        break; 
+                    }
+                }
+            }
+        }
+        writeBlock++;
+    }
+
+    // Write any remaining bytes to last block
+    if (*bytesToWrite > 0) {
+        block_read(writeBlock, bounceBuffer);
+        memcpy(bounceBuffer, buf, *bytesToWrite);
+        bytesWritten += *bytesToWrite;
+        *bytesToWrite -= *bytesToWrite;
+        block_write(writeBlock, bounceBuffer);
+    }   
+
+    for (int i = 0; i < FS_FILE_MAX_COUNT; i++) {
+    	if(!strcmp(root[i].filename, fileDescriptors[fd].filename) && fileDescriptors[fd].offset + bytesWritten > root[i].size) {
+            root[i].size = fileDescriptors[fd].offset + bytesWritten;
+            break;
+        }
+    }           
+	
+    free(bounceBuffer);
+    free(bytesToWrite);
+
+    fileDescriptors[fd].offset += bytesWritten;
+    buf -= bytesWritten;
+
+	return bytesWritten;
 }
 
 int fs_read(int fd, void *buf, size_t count)
 {
     int* bytesToRead = (int *)malloc(sizeof(count));
-    int readBlock, blockBytes, bytesRead = 0;
+    int readBlock, blockBytes, blockOffset, bytesRead = 0;
     void* bounceBuffer = NULL;
     if (fd >= FS_OPEN_MAX_COUNT || fd < 0 || fileDescriptors[fd].fd != fd) {
             return -1;
     }
 
     *bytesToRead = count;
-    readBlock = find_dataBlock(fd, bytesToRead);
-    
-    //TODO: test this case
-    if (fileDescriptors[fd].offset % BLOCK_SIZE != 0) { // Offset is in middle of a block
+    readBlock = find_dataBlock(fd, bytesToRead, READ);
+    blockOffset = fileDescriptors[fd].offset - (fileDescriptors[fd].offset/BLOCK_SIZE)*BLOCK_SIZE;
+ 
+    if (blockOffset != 0) { // Offset is in middle of a block
         bounceBuffer = malloc(BLOCK_SIZE);
         block_read(readBlock, bounceBuffer);
-        blockBytes = fileDescriptors[fd].offset - BLOCK_SIZE*readBlock; // Bytes to read from first block
+        blockBytes = (fileDescriptors[fd].offset/BLOCK_SIZE + 1)*BLOCK_SIZE - fileDescriptors[fd].offset; // Bytes to read from first block
         if (*bytesToRead < blockBytes) { // Only need to read from this one block
-            memcpy(buf, bounceBuffer, *bytesToRead);
+            memcpy(buf, bounceBuffer + blockOffset, *bytesToRead);
             *bytesToRead -= *bytesToRead; // bytesToRead = 0
             bytesRead += *bytesToRead;
         }
         else { // Read to the end of the block
-            memcpy(buf, bounceBuffer, blockBytes);
+            memcpy(buf, bounceBuffer + blockOffset, blockBytes);
             readBlock++;
             bytesRead += blockBytes;
             *bytesToRead -= blockBytes;
-            buf += blockBytes; //TODO: maybe don't modify buf directly; use a temp pointer initialized to buf instead
+            buf += blockBytes;
             memset(bounceBuffer, 0 , BLOCK_SIZE); // Clear the bounce buffer for possible later use
         }
     }
 
     // Read whole blocks until no longer possible
-    while (*bytesToRead >= BLOCK_SIZE) { //TODO: test this case
+    while (*bytesToRead >= BLOCK_SIZE) {
         block_read(readBlock, buf);
         *bytesToRead -= BLOCK_SIZE;
         bytesRead += BLOCK_SIZE;
-        buf += BLOCK_SIZE; //TODO: change buf to temp?
+        buf += BLOCK_SIZE;
         readBlock++;
     }
 
     // Read any remaining bytes
-    if (*bytesToRead != 0) {
+    if (*bytesToRead > 0) {
         if (!bounceBuffer)
             bounceBuffer = malloc(BLOCK_SIZE);
         block_read(readBlock, bounceBuffer);
@@ -365,7 +460,9 @@ int fs_read(int fd, void *buf, size_t count)
     if (bounceBuffer)
         free(bounceBuffer);
   
-    fileDescriptors[fd].offset += bytesRead; 
+    free(bytesToRead);
+    fileDescriptors[fd].offset += bytesRead;
+    buf -= bytesRead; 
 	return bytesRead;
 }
 
